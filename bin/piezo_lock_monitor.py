0b24e61dc255fa0c125b493de28998fc74de9edd
lexie <zwang833@wisc.edu>
2018-05-18 15:55:57 -0500

    add email alerts when errors detected.

--- bin/piezo_lock_monitor.py
+++ bin/piezo_lock_monitor.py
@@ -7,18 +7,32 @@ import pprint
 import logging
 import time
 import numpy as np
-import warnings
+import requests
 from origin.client.origin_subscriber import Subscriber
 
 
 # define a custom function to pass to the poller loop
+#send email messages function
+def send_messages(ch,status,adrs):
+    """adrs is the email address receiving messages,
+    ch is the channel number,
+    status is string value unlocked/relocked/out of range"""
+    return requests.post(
+        "https://api.mailgun.net/v3/sandbox614407adee87476b873974bc39be24f9.mailgun.org/messages",
+        auth=("api", "key-1ecf921dc7a279fb94acc63130f0775d"),
+        data={
+            "from": "Excited User <mailgun@sandbox614407adee87476b873974bc39be24f9.mailgun.org>",
+            "to": [adrs],
+            "subject": ch+' is '+status,
+            "text": ch+' is '+status})
+
 # MUST BE DEFINED BEFORE SUBSCRIBER INSTANTIATION
-def piezo_monitor(stream_id, data, state, log, buflen=100, trigstd=3, init=30, ch=None ,filename=None):
+def piezo_monitor(stream_id, data, state, log, buflen=100, trigstd=3, init=30, ch=None ,filename=None,adrs=None):
     """
     buflen is the length of the circular buffer
     trigstd is units of std deviation from average
     init is the index of initial elements filled in buffer before sending alarm
-    ch is the channel number,data[ch]=float
+    ch is the channel number,ch=str,data[ch]=float,
     filename is name of saved csv file
     """
     skip = False
@@ -33,11 +47,15 @@ def piezo_monitor(stream_id, data, state, log, buflen=100, trigstd=3, init=30, c
         skip = True
     try:
         if not skip: #exclude initialization index
-            if state['error'] == False: #no error occured
-                #warning if data is detected to be out of range,skip alarm and keep adding new data in
+            if state['error'] == False: #no error occured in last data
+                #warning if data is detected to be out of range,skip alarm and dont put in data untill it's in range
                 if data[ch] in [0,4095]:
-                    log.warning('Out of range')
+                    log.warning(ch+' Out of range')
                     state['error'] = True
+                    try:
+                        send_messages(ch,'out of range',adrs)
+                    except Exception:
+                        log.warning('Input adress as a string')
                 else:
                     #after initialization, calculate mean and std of previous data
                     mean = np.nanmean(state['prev_data'])
@@ -50,23 +68,26 @@ def piezo_monitor(stream_id, data, state, log, buflen=100, trigstd=3, init=30, c
                             log.info(ch+' unlock!?')
                             #the time of unlock alarm
                             t = time.time()
-                            state['time'].append(t)
-                            #state['unlock_voltage'].append(data[ch])
-                            log.info(state['time'])
-                            log.info("[{}]: {}".format(stream_id, state))
-                            #log.info(state['unlock_voltage'])
+                            log.debug("[{}]: {}".format(stream_id, state))
                             with open(filename,'a') as f:
                                 f.write(str(t)+'\n')
-                                #np.savetxt(f,np.column_stack(state['time'],state['unlock_voltage']))
-                #put new data into memory (prev_data)
-                state['index'] += 1
-                #cycle index if it gets to buffer length
-                if state['index'] > (buflen-1):
-                    state['index'] = 0
+                            try:
+                                send_messages(ch,'unlocked',adrs)
+                            except Exception:
+                                log.warning('Input adress as a string')
+                    # putting new data into memory
+                    state['index'] += 1
+                    #cycle index if it gets to buffer length
+                    if state['index'] > (buflen-1):
+                        state['index'] = 0
             else: #state['error'] = True, error occured
-                if (data[ch]-2048)< 500:
+                if abs(data[ch]-2048)< 500:
                     state['error'] = False
                     log.info('relocked')
+                    try:
+                        send_messages(ch,'relocked',adrs)
+                    except Exception:
+                        log.warning('Input adress as a string')
 
     except KeyError:
         log.error('Problem accessing key `c3`. Are you subscribed to the right stream?')
@@ -77,11 +98,17 @@ def piezo_monitor(stream_id, data, state, log, buflen=100, trigstd=3, init=30, c
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
+
     ch = logging.StreamHandler()
     ch.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     logger.addHandler(ch)
+
+    fLog = logging.FileHandler("f.log")
+    fLog.setLevel(logging.DEBUG)
+    fLog.setFormatter(formatter)
+    logger.addHandler(fLog)
 
     # first find ourself
     fullBinPath = os.path.abspath(os.getcwd() + "/" + sys.argv[0])
@@ -119,7 +146,7 @@ if __name__ == "__main__":
     # parameters through kwargs
     sub.subscribe(stream, callback=piezo_monitor, buflen=200, trigstd=12, init=30, ch='c3',filename='RbMOT.csv')
     sub.subscribe(stream, callback=piezo_monitor, buflen=200, trigstd=15, init=30, ch='c4',filename='RbHF.csv')
-    sub.subscribe(stream, callback=piezo_monitor, buflen=200, trigstd=6, init=30, ch='c5',filename='CsHF.csv')
+    sub.subscribe(stream, callback=piezo_monitor, buflen=200, trigstd=6, init=30, ch='c5',filename='CsHF.csv',adrs='zwang833@wisc.edu')
 
     try:
         while True:
